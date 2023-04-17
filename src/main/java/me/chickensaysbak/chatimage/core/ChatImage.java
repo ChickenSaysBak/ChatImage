4b3d1606a9b5d6df1d3950996003c4f97dd3ca67
ChickenSaysBak <nicholash2000@icloud.com>
2023-04-17 17:07:15 -0400

    Added loadImage() and stripURL()

--- src/main/java/me/chickensaysbak/chatimage/core/ChatImage.java
+++ src/main/java/me/chickensaysbak/chatimage/core/ChatImage.java
@@ -8,9 +8,16 @@ import me.chickensaysbak.chatimage.core.commands.ChatImageCommand;
 import me.chickensaysbak.chatimage.core.commands.IgnoreImagesCommand;
 import net.md_5.bungee.api.ChatColor;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.UUID;
 
 public class ChatImage {
+
+    public static final String[] SUPPORTED_EXTENSIONS = new String[] {".png", ".jpg", ".jpeg", ".gif"};
 
     private static ChatImage instance;
     private PluginAdapter plugin;
@@ -74,6 +81,61 @@ public class ChatImage {
     public void reload() {
         settings.reload();
         ignoringImages.reload();
+    }
+
+    /**
+     * Loads an image.
+     * @param url the url string of the image
+     * @return the loaded image or null if it failed to load
+     */
+    public BufferedImage loadImage(String url) {
+
+        url = stripURL(url);
+
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            // Prevents 403 Forbidden errors.
+            connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+            return ImageIO.read(connection.getInputStream());
+        }
+
+        catch (IOException e) {
+
+            if (settings.isDebug()) {
+                plugin.getLogger().warning("ChatImage Debugger - Error loading image");
+                plugin.getLogger().warning("URL: " + url);
+                e.printStackTrace();
+            }
+
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Gets rid of extraneous specifications after the file extension of an image url.
+     * Helps to prevent incompatible links in moderatecontent.com
+     * @param url the url to strip
+     * @return the image url without extra parameters after the extension
+     */
+    public static String stripURL(String url) {
+
+        String[] split1 = url.split("//", 2);
+        if (split1.length < 2) return url;
+        String urlMiddle = split1[1]; // Everything after the protocol (http or https).
+
+        String[] split2 = urlMiddle.split("/", 2);
+        if (split2.length < 2) return url;
+        String urlEnd = split2[1]; // Everything after the domain.
+
+        for (String ext : SUPPORTED_EXTENSIONS) if (urlEnd.contains(ext)) {
+            urlEnd = urlEnd.split(ext)[0] + ext;
+            break;
+        }
+
+        return split1[0] + "//" + split2[0] + "/" + urlEnd;
+
     }
 
     public static ChatImage getInstance() {
