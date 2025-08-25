0edc38535ddb1e0f7f66b63028aff0e6555ce606
ChickenSaysBak <nicholash2000@icloud.com>
2025-08-24 20:44:07 -0400

    Translate dot separated YAML paths into arrays for Velocity

--- chatimage-velocity/src/main/java/me/chickensaysbak/chatimage/velocity/YamlVelocity.java
+++ chatimage-velocity/src/main/java/me/chickensaysbak/chatimage/velocity/YamlVelocity.java
@@ -31,7 +31,7 @@ public class YamlVelocity implements YamlAdapter {
     public void set(String path, Object value) {
 
         try {
-            config.node(path).set(value);
+            nodeAtDotPath(config, path).set(value);
         } catch (SerializationException e) {
             plugin.getLogger().log(Level.SEVERE, e.getMessage(), e);
         }
@@ -45,44 +45,55 @@ public class YamlVelocity implements YamlAdapter {
 
     @Override
     public Collection<String> getKeys(String path) {
-        return config.node(path).childrenMap().keySet().stream().map(Object::toString).collect(Collectors.toList());
+        return nodeAtDotPath(config, path).childrenMap().keySet().stream().map(Object::toString).collect(Collectors.toList());
     }
 
     @Override
     public boolean getBoolean(String path, boolean def) {
-        return config.node(path).getBoolean(def);
+        return nodeAtDotPath(config, path).getBoolean(def);
     }
 
     @Override
     public int getInt(String path, int def) {
-        return config.node(path).getInt(def);
+        return nodeAtDotPath(config, path).getInt(def);
     }
 
     @Override
     public long getLong(String path, long def) {
-        return config.node(path).getLong(def);
+        return nodeAtDotPath(config, path).getLong(def);
     }
 
     @Override
     public double getDouble(String path, double def) {
-        return config.node(path).getDouble(def);
+        return nodeAtDotPath(config, path).getDouble(def);
     }
 
     @Override
     public String getString(String path, String def) {
-        return config.node(path).getString(def);
+        return nodeAtDotPath(config, path).getString(def);
     }
 
     @Override
     public List<String> getStringList(String path) {
 
         try {
-            return config.node(path).getList(String.class);
+            return nodeAtDotPath(config, path).getList(String.class);
         } catch (SerializationException e) {
             plugin.getLogger().log(Level.SEVERE, e.getMessage(), e);
             return List.of();
         }
 
+    }
+
+    /**
+     * Gets the node at the specified dot.separated.path.
+     * @param root the node to start from
+     * @param path the path separated by dots
+     * @return the node at the destination of the path
+     */
+    private static ConfigurationNode nodeAtDotPath(ConfigurationNode root, String path) {
+        String[] parts = path.split("\\.");
+        return root.node((Object[]) parts);
     }
 
 }
